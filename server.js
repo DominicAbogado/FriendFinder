acb93dd3fa15a61cdecc03d68783bfc5641d2d53
DominicAbogado <dominic.j.abogado@gmail.com>
2018-07-08 13:27:55 -0400

    doing the easy stuff

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+//sets up the Express App
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+  });
+  
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+  });
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
+  
