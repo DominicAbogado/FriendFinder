0441ed66c6f375a923d222af88bcb6ca005c82d5
DominicAbogado <dominic.j.abogado@gmail.com>
2018-07-09 17:21:34 -0400

    server working and giving the pages

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        name: "Dominic Abogado",
+        photo: "https://drive.google.com/file/d/1ma2AazYuxWjC07kFUI63w0mBe-f47mI_/view?usp=sharing",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+]
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
+
+
 //sets up the Express App
 
 var app = express();
@@ -10,16 +12,25 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-  });
-  
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-  });
 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+//Grabbing routes from my routes page
+// require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+
+app.listen(PORT, function(){
+  console.log("App listening on PORT " + PORT);
+})
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/home.html"));
+//   });
+  
+//   app.get("/survey", function(req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+//   });
+
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+//   });
   
   
